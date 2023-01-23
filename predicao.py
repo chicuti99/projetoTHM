25c0c11b58153663df3ec63a6d0b3bffe8f77a47
chicuti99 <arthurchicuti0@gmail.com>
2023-01-23 15:41:27 -0300

    Update predicao.py

--- predicao.py
+++ predicao.py
@@ -5,31 +5,30 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 import sqlite3
 
-# connect to the database
+
 conn = sqlite3.connect('registros.db')
 
-# load data from the database
+
 data = pd.read_sql_query("SELECT name, time, location, flood_intensity, classification FROM flooding_data", conn)
 
-# define input and output variables
+
 X = data[['name', 'time', 'location', 'flood_intensity']]
 y = data['classification']
 
-# split the data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# train the model
+
 clf = RandomForestClassifier(n_estimators=100, random_state=42)
 clf.fit(X_train, y_train)
 
-# make predictions on the test set
+
 y_pred = clf.predict(X_test)
 
-# evaluate the model
+
 accuracy = accuracy_score(y_test, y_pred)
 print('Accuracy:', accuracy)
 
-# Plotting the confusion matrix
+
 confusion_mat = confusion_matrix(y_test, y_pred)
 plt.imshow(confusion_mat, cmap='Blues', interpolation='nearest')
 plt.colorbar()
@@ -40,5 +39,5 @@ plt.xlabel('Predicted')
 plt.ylabel('True')
 plt.show()
 
-# close the connection to the database
+
 conn.close()
